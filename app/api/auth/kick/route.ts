8a9e38c823dd67452a7bd9f954cb4e84dfab783a
staff1g <nawara465@gmail.com>
2026-06-30 07:01:36 +0200

    Try dash scope name

--- app/api/auth/kick/route.ts
+++ app/api/auth/kick/route.ts
@@ -24,7 +24,7 @@ export async function GET() {
 
     redirect_uri: redirectUri,
     response_type: 'code',
-    scope: 'user:read channel:read events:subscribe channel:write channel_points.write channel_points.read',
+    scope: 'user:read channel:read events:subscribe channel:write channel-points:read channel-points:write',
     code_challenge: codeChallenge,
     code_challenge_method: 'S256',
     state: state,
